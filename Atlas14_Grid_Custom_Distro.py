13bbe42f0344f9f38d7e86decd753440790d4136
Myles McManus <mrmylesmc@gmail.com>
2025-07-22 10:35:22 -0500

    ran with custom distro to produce netCDF files

--- Atlas14_Grid_Custom_Distro.py
+++ Atlas14_Grid_Custom_Distro.py
@@ -10,10 +10,10 @@ import os
 import geopandas as gpd
 
 # setup project dir data if custom merged diffrent from the Atlas 14 grid extent by region
-project_name = 'Lawton'
-clip_shp = "boundaries\Lawton_BB_StatePlaneOK.shp"
+project_name = 'I57_'
+clip_shp = "boundaries/I57_BB_3857.shp"
 # merged will be a merge of multiple Atlas 14 grids and clipped to the project extent already.
-merged = False
+merged = True
 
 region = {
     'name': 'Midwest',
@@ -122,6 +122,7 @@ for i, year in enumerate(years_padded):
                 'duration': dur
             }
         else:
+            # grids must be previously  merged using grid_merge_clip.py
             grids[f'{year}yr_Partial_Duration_{dur}Precip'] = {
                 'path': f'data/Merged/{project_name}/{project_name}{year}yr{dur}a.asc',
                 'year': year,
@@ -151,89 +152,89 @@ for grid in grids:
         da = da.rio.clip(clip_gdf.geometry.apply(mapping),
                         crs=clip_gdf.crs, drop=True, all_touched=True)
         
-        for tables_name, table in tables.items():
-            print(f'Applying {tables_name} temporal distribution...')
-            # put the temporal distribution table into a pandas DataFrame
-            df_table = pd.DataFrame.from_dict(table, orient='index', columns=['value'])
-            df_table.index.name = 'hours'
-            df_table.reset_index(inplace=True)
+    for tables_name, table in tables.items():
+        print(f'Applying {tables_name} temporal distribution...')
+        # put the temporal distribution table into a pandas DataFrame
+        df_table = pd.DataFrame.from_dict(table, orient='index', columns=['value'])
+        df_table.index.name = 'hours'
+        df_table.reset_index(inplace=True)
 
-            # collecting data arrays for each timestep to stack into a single dataset.
-            list_da = []
-            start_time = datetime.datetime(2000, 1, 1, 0, 0, 0)
-            
-            for index, row in df_table.iterrows():
-                # calculate the time for each row
-                timestep = start_time + datetime.timedelta(hours=row['hours'])
-                da_copy = da.copy(deep=True)
-            
-                # Convert Units to the {temporal_value_occurrence_column} Occurance Temporal Value increment to create a dataarray to be stacked into a dataset with a time dimension.
-                da_copy = da_copy*(df_table['value'].iloc[index]/100.0)  # Convert percentage to decimal
-                #  Rename data array data variable
-                da_copy = da_copy.rename('PrecipCumulative')
-                # Assign time coordinate  
-                da_copy = da_copy.assign_coords(time = timestep)
-                da_copy = da_copy.expand_dims(dim="time")
-                # Append to list
-                list_da.append(da_copy)
+        # collecting data arrays for each timestep to stack into a single dataset.
+        list_da = []
+        start_time = datetime.datetime(1970, 1, 1, 0, 0, 0)
+        
+        for index, row in df_table.iterrows():
+            # calculate the time for each row
+            timestep = start_time + datetime.timedelta(hours=row['hours'])
+            da_copy = da.copy(deep=True)
+        
+            # Convert Units to the {temporal_value_occurrence_column} Occurance Temporal Value increment to create a dataarray to be stacked into a dataset with a time dimension.
+            da_copy = da_copy*(df_table['value'].iloc[index]/100.0)  # Convert percentage to decimal
+            #  Rename data array data variable
+            da_copy = da_copy.rename('PrecipCumulative')
+            # Assign time coordinate  
+            da_copy = da_copy.assign_coords(time = timestep)
+            da_copy = da_copy.expand_dims(dim="time")
+            # Append to list
+            list_da.append(da_copy)
 
-            # stack the dataarrays into a single dataset.
-            ds = xr.concat(list_da, dim="time")
-            ds = ds.to_dataset(name='PrecipCumulative')
-            
-            # Remove band dimension.
-            ds = ds.squeeze()
-            ds = ds.drop_vars('band')
+        # stack the dataarrays into a single dataset.
+        ds = xr.concat(list_da, dim="time")
+        ds = ds.to_dataset(name='PrecipCumulative')
+        
+        # Remove band dimension.
+        ds = ds.squeeze()
+        ds = ds.drop_vars('band')
 
-            # Create Precip Incremental variable
-            ds['PrecipInc'] = ds['PrecipCumulative'].diff(dim='time', label='upper')
+        # Create Precip Incremental variable
+        ds['PrecipInc'] = ds['PrecipCumulative'].diff(dim='time', label='upper')
 
-            # CF Conventions
-            ds = ds.rename({
-                'x':'longitude',
-                'y':'latitude'
-            })
+        # CF Conventions
+        ds = ds.rename({
+            'x':'longitude',
+            'y':'latitude'
+        })
 
-            ds['latitude'].attrs['units'] = 'degrees_north'
-            ds['latitude'].attrs['standard_name'] = 'latitude'
-            ds['latitude'].attrs['long_name'] = 'latitude'
-            ds['latitude'].attrs['axis'] = 'Y'
+        ds['latitude'].attrs['units'] = 'degrees_north'
+        ds['latitude'].attrs['standard_name'] = 'latitude'
+        ds['latitude'].attrs['long_name'] = 'latitude'
+        ds['latitude'].attrs['axis'] = 'Y'
 
-            ds['longitude'].attrs['units'] = 'degrees_east'
-            ds['longitude'].attrs['standard_name'] = 'longitude'
-            ds['longitude'].attrs['long_name'] = 'longitude'
-            ds['longitude'].attrs['axis'] = 'X'
+        ds['longitude'].attrs['units'] = 'degrees_east'
+        ds['longitude'].attrs['standard_name'] = 'longitude'
+        ds['longitude'].attrs['long_name'] = 'longitude'
+        ds['longitude'].attrs['axis'] = 'X'
 
-            ds['time'].attrs['standard_name'] = 'time'
-            ds['time'].attrs['long_name'] = 'time'
-            ds['time'].attrs['axis'] = 'T'
+        ds['time'].attrs['standard_name'] = 'time'
+        ds['time'].attrs['long_name'] = 'time'
+        ds['time'].attrs['axis'] = 'T'
 
-            ds['PrecipCumulative'].attrs['units'] = 'inches'
-            ds['PrecipCumulative'].attrs['long_name'] = 'Cumulative Precipitation'
-            
-            ds['PrecipInc'].attrs['units'] = 'inches'
-            ds['PrecipInc'].attrs['long_name'] = 'Incremental Precipitation'
-            
-            # Add temporal distribution to the dataset.
-            ds_td = df_table.to_xarray()
-            ds_td.expand_dims(dim="time")
-            ds_td["time"] = ds.time
-            ds_td["value"] = ds_td["value"].swap_dims({"index":"time"})
-            ds_td = ds_td.drop_vars("hours")
-            ds_td = ds_td.drop_vars("index")
-            ds_td = ds_td.rename({"value":"TemporalDistribution"})
-            ds = xr.merge([ds,ds_td])
-            ds['TemporalDistribution'].attrs['units'] = 'percent'
-            ds['TemporalDistribution'].attrs['long_name'] = 'Temporal Distribution Cumulative Percentage'
-            ds['TemporalDistribution'].attrs['temporalDuration'] = temporal_duration_name
-            ds['TemporalDistribution'].attrs['source'] = f'{tables_name} Temporal Distribution Table'
+        ds['PrecipCumulative'].attrs['units'] = 'inches'
+        ds['PrecipCumulative'].attrs['long_name'] = 'Cumulative Precipitation'
+        
+        ds['PrecipInc'].attrs['units'] = 'inches'
+        ds['PrecipInc'].attrs['long_name'] = 'Incremental Precipitation'
+        
+        # Add temporal distribution to the dataset.
+        ds_td = df_table.to_xarray()
+        ds_td.expand_dims(dim="time")
+        ds_td["time"] = ds.time
+        ds_td["value"] = ds_td["value"].swap_dims({"index":"time"})
+        ds_td = ds_td.drop_vars("hours")
+        ds_td = ds_td.drop_vars("index")
+        ds_td = ds_td.rename({"value":"TemporalDistribution"})
+        ds = xr.merge([ds,ds_td])
+        ds['TemporalDistribution'].attrs['units'] = 'percent'
+        ds['TemporalDistribution'].attrs['long_name'] = 'Temporal Distribution Cumulative Percentage'
+        ds['TemporalDistribution'].attrs['temporalDuration'] = temporal_duration_name
+        ds['TemporalDistribution'].attrs['source'] = f'{tables_name} Temporal Distribution Table'
 
-            # Export to netCDF
-            output_file = rf"output\{project_name}\nc\Atlas14_{project_name}{grids[grid]['year_int']}yr_{temporal_duration_name}_{tables_name}.nc"
-            # create output directory if it does not exist
-            os.makedirs(os.path.dirname(output_file), exist_ok=True)
-            print(f'Exporting to {output_file}')
-            ds.to_netcdf(output_file)
+        # Export to netCDF
+        output_file = rf"output\{project_name}\nc\Atlas14_{project_name}{grids[grid]['year_int']}yr_{temporal_duration_name}_{tables_name}.nc"
+        # create output directory if it does not exist
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        print(f'Exporting to {output_file}\n')
+        ds.to_netcdf(output_file)
 
                 
 # %%
